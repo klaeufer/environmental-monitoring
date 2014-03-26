d8ea8948b8dc52a4188d0d9e3ae73af645835ba1
Asraf Alom <malom@luc.edu>
2014-03-25 19:19:47 -0700

    creating and openning interfacekit

---
+++ get-sensor-data/get-sensor-data.py
@@ -0,0 +1,29 @@
+'''
+Created on Mar 25, 2014
+
+@author: asraf
+'''
+
+from Phidgets.PhidgetException import PhidgetErrorCodes, PhidgetException
+from Phidgets.Devices.InterfaceKit import InterfaceKit
+
+
+# Create InterfaceKit device
+try:
+    device = InterfaceKit()
+    print("InterfaceKit created..")
+except RuntimeError as e:
+    print("Runtime error: %s" % e.message)
+    
+
+# Open it
+try:
+    device.openPhidget()
+    print("InterfaceKit opened..")
+except PhidgetException as e:    
+    print("Phidget exception %i: %s" % (e.code, e.detail))
+
+
+# Wait for device attachment
+device.waitForAttach(10000)
+print("Device(%d) attached!" % (device.getSerialNum()))
