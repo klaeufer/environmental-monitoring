95e032d7e8316c9d58e79372b2e98b47e0997356
Michael Dotson <mdotson1@mdotson1-Aspire-7750G.(none)>
2014-04-01 15:41:17 -0500

    getting prepared for heroku

---
+++ routes.py
@@ -0,0 +1,11 @@
+import os
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def default():
+	if request.method == 'POST':
+		return "you posted!"
+	if request.method == 'GET':
+		return "GET, good job!"
