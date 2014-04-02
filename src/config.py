3d18faa4a1336f6cdd82c94e8b152771f91e69a3
Asraf Alom <malom@luc.edu>
2014-04-02 05:05:34 -0500

    added comments

--- src/config.py
+++ src/config.py
@@ -4,7 +4,9 @@ Created on Apr 04, 2014
 Configuration file to set up various fixed values
 '''
 
-URL = 'http://environmental-monitoring.herokuapp.com/'
-WAIT_TIME = 10000
+URL = 'http://environmental-monitoring.herokuapp.com/'      # Server
+
+WAIT_TIME = 10000                   # Wait time to attach the Phidget
+
 TEMPERATURE_TRIGGERING_POINT = 1    # Event triggered if raw-data changed by 1 point
 HUMIDITY_TRIGGERING_POINT = 1       # Event triggered if raw-data changed by 1 point
