b535efc6f2d70015be2357f3c7af12cae53760e1
Asraf Alom <malom@luc.edu>
2014-04-02 05:13:23 -0500

    adding more constants

--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
 '''
 Created on Apr 04, 2014
 @author: asraf
-Configuration file to set up various fixed values
+Global file to set up various fixed values
 '''
 
 URL = 'http://environmental-monitoring.herokuapp.com/'      # Server
 
 WAIT_TIME = 10000                   # Wait time to attach the Phidget
+OUT_FILE = 'sensor-data.csv'
+CSV_HEADER = 'Time, Temperature(C), Humidity(%) \n'
 
 TEMPERATURE_TRIGGERING_POINT = 1    # Event triggered if raw-data changed by 1 point
 HUMIDITY_TRIGGERING_POINT = 1       # Event triggered if raw-data changed by 1 point
