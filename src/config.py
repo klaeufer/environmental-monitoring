bc4588817ad7f4fb874c38450351f801496d0b76
Asraf Alom <malom@luc.edu>
2014-04-02 05:04:25 -0500

    moving constants to a config file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+'''
+Created on Apr 04, 2014
+@author: asraf
+Configuration file to set up various fixed values
+'''
+
+URL = 'http://environmental-monitoring.herokuapp.com/'
+WAIT_TIME = 10000
+TEMPERATURE_TRIGGERING_POINT = 1    # Event triggered if raw-data changed by 1 point
+HUMIDITY_TRIGGERING_POINT = 1       # Event triggered if raw-data changed by 1 point
